01f4ad8dd5c777604121548994f8374c8f28a2ec
mzhr <mzhr@openmailbox.org>
2015-11-11 16:18:45 +1100

    Added new text note activity.

--- app/src/main/java/mzhr/librenote/activities/MainActivity.java
+++ app/src/main/java/mzhr/librenote/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package mzhr.librenote.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import mzhr.librenote.R;
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newTextNote(View view) {
+        Intent newIntent = new Intent(MainActivity.this, TextNoteActivity.class);
+        startActivity(newIntent);
     }
 }
---
+++ app/src/main/java/mzhr/librenote/activities/TextNoteActivity.java
@@ -0,0 +1,15 @@
+package mzhr.librenote.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import mzhr.librenote.R;
+
+public class TextNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_note);
+    }
+}
