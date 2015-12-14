4d0b7ad36574c3ccef87a37ea9bd65c1acbbd39d
mzhr <mzhr@openmailbox.org>
2015-12-14 20:58:06 +1100

    Added functions for deleting rewriting and reading files. Functions need to mapped to actions later.

--- app/src/main/java/mzhr/librenote/models/NoteStorage.java
+++ app/src/main/java/mzhr/librenote/models/NoteStorage.java
@@ -2,8 +2,13 @@ package mzhr.librenote.models;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
  * Class for processing file accessing, creation, deletion and editing.
@@ -16,14 +21,10 @@ public class NoteStorage {
             context.openFileInput(name);
             throw new Exception("Cannot create file. File with same name already exists.");
         } catch (IOException exist_e) {
-            FileOutputStream outputStream;
-            try {
-                outputStream = context.openFileOutput(name, Context.MODE_PRIVATE);
-                outputStream.write(content.getBytes());
-                outputStream.close();
-            } catch (Exception write_e) {
-                throw write_e;
-            }
+            FileOutputStream outputStream = context.openFileOutput(name, Context.MODE_PRIVATE);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+            writer.write(content);
+            writer.close();
         }
     }
 
@@ -32,21 +33,45 @@ public class NoteStorage {
         return context.fileList();
     }
 
-    /*
-    public String openTextNote(Context context, String name, String content)
-    {
 
+    public String getTextNote(Context context, String name) {
+        FileInputStream inputStream;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(context.openFileInput(name)));
+        } catch (IOException open_e) {
+            open_e.printStackTrace();
+        }
+        String line;
+        StringBuffer buffer = new StringBuffer();
+        try {
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+        } catch (IOException read_e) {
+            read_e.printStackTrace();
+        }
+        String content = buffer.toString();
+        return content;
     }
 
     public boolean editTextNote(Context context, String name, String content)
     {
-
+        try {
+            FileOutputStream outputStream = context.openFileOutput(name, Context.MODE_PRIVATE);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
-    public boolean removeTextNote(Context context, String name)
+    public void removeTextNote(Context context, String name)
     {
-
+            context.deleteFile(name);
     }
-    */
 
 }
